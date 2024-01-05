20e31db2eecfd7b55c599dd17fb9b6cad9186608
Daksh <akshd@outlook.com>
2024-01-05 22:35:21 +0530

    init with dark more

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from '@/components/ui/themeProvider'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark">
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </div>
+  )
 }
