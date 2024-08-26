76f6ff9ac6af0a6928867af3c216ffa7464bf2f5
daksh <akshd@outlook.com>
2024-08-26 20:47:58 +0530

    feat: add nice bg gradient

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    <div
+      className={`flex min-h-screen flex-col items-center bg-custom-radial justify-between p-24 ${inter.className}`}
     >
-    </main>
+    </div>
   )
 }
